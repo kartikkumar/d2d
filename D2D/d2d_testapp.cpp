56e3789e3be90b5bee80d68eb0d7cbecdea73ceb
kumar <kumar@8168e3c5-a2ef-4d05-a50c-0815c6ab60aa>
2014-04-23 16:34:56 +0000

    Add latest version of code.
    git-svn-id: https://dinamicatech.dnsdynamic.com/svn/kumar/d2d/trunk@6 8168e3c5-a2ef-4d05-a50c-0815c6ab60aa

---
+++ D2D/d2d_testapp.cpp
@@ -0,0 +1,38 @@
+ /*
+  * Copyright (c) 2014, Dinamica Srl
+  * Copyright (c) 2014, K. Kumar (kumar@dinamicatech.com)
+  * All rights reserved.
+  * See COPYING for license details.
+  */
+
+#include <cstdlib>
+#include <iostream>
+
+#include <boost/timer/timer.hpp>  
+
+#include <sgp4/cpp/sgp4unit.h>
+
+//! Execute D2D test app.
+int main( const int numberOfInputs, const char* inputArguments[ ] )
+{
+    ///////////////////////////////////////////////////////////////////////////
+
+    // Start timer. Timer automatically ends when this object goes out of scope.
+    boost::timer::auto_cpu_timer timer;
+
+    ///////////////////////////////////////////////////////////////////////////
+     
+    ///////////////////////////////////////////////////////////////////////////
+
+    // Declare using-statements.
+    using std::cerr;
+    using std::cout;
+    using std::endl;
+
+    ///////////////////////////////////////////////////////////////////////////
+                
+    cout << "Timing information: ";
+
+    // If program is successfully completed, return 0.
+    return EXIT_SUCCESS; 
+}
---
+++ README.md
@@ -0,0 +1,38 @@
+Debris-2-Debris
+===================
+
+`Debris-2-Debris` (`D2D`) is a toolbox that can be used to rapidly search through a catalog of space debris objects to find candidate optimal debris-to-debris transfers. These candidate pairs can be fed into a full-scale optimization problem in the context of mission analysis for an Active Debris Removal (ADR) space mission.
+
+For an example use of this toolbox, take a look at (Kumar et al., 2014). 
+
+Requirements
+------
+
+Installation
+------
+
+
+Documentation
+-------------
+
+You can pass the `-DBUILD_DOCUMENTATION=on` option to `CMake` to build the [Doxygen](http://www.doxygen.org "Doxygen homepage") documentation locally.
+
+Contributing
+------------
+
+Contact me at [kumar@dinamicatech.com](kumar@dinamicatech.com) to find out how you can contribute to the `D2D` project.
+
+License
+------
+
+See `COPYING`.
+
+Disclaimer
+------
+
+Dinamica Srl is not liable for any damage(s) incurred due to improper use of `D2D`.
+
+Contact
+------
+
+You can reach me at [kumar@dinamicatech.com](kumar@dinamicatech.com).
