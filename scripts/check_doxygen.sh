4be765360e028bb0245a47504bea4798f7fc35c7
Kartik Kumar <me@kartikkumar.com>
2015-02-18 15:58:01 +0100

    Fix Doxygen build instructions in Travis using an external shell script.

---
+++ scripts/check_doxygen.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+set -ev
+
+# Count the number of warnings when building Doxygen output.
+warnings=$(make doxygen_docs 2>&1 >/dev/null | wc -l)
+if [[ $warnings -ne 0 ]]; then
+  # Print the output.
+  make doxygen_docs;
+  echo "Found Doxygen warnings";
+  exit 1;
+fi;
