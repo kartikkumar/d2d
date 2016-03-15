5038ffd8688b714e1b5c59672ea2f310836a534f
Abhishek Agrawal <abhishek.agrawal@protonmail.com>
2016-03-15 14:08:19 +0100

    Add copyright statement.

--- include/D2D/tools.hpp
+++ include/D2D/tools.hpp
@@ -1,5 +1,6 @@
 /*
  * Copyright (c) 2014-2016 Kartik Kumar, Dinamica Srl (me@kartikkumar.com)
+ * Copyright (c) 2014-2016 Abhishek Agrawal, Delft University of Technology (abhishek.agrawal@protonmail.com)
  * Distributed under the MIT License.
  * See accompanying file LICENSE.md or copy at http://opensource.org/licenses/MIT
  */
