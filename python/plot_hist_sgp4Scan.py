aaa4c92083b3f7101abcb0bd9c3f97edaecfa96b
Abhishek Agrawal <abhishek.agrawal@protonmail.com>
2016-04-05 23:31:34 +0200

    Add feature for ECI to RTN frame conversion

--- python/plot_hist_sgp4Scan.py
+++ python/plot_hist_sgp4Scan.py
@@ -22,6 +22,7 @@ import pandas as pd
 # System
 import sys
 import time
+from tqdm import tqdm
 
 print ""
 print "---------------------------------------------------------------------------------"
@@ -101,6 +102,29 @@ scan_data = pd.read_sql( "SELECT    " + config['error'] + "_x_error,            
 
 scan_data.columns = [ 'xerror', 'yerror', 'zerror', 'magnitudeError' ]
 
+lambert_scan_data = pd.read_sql( "SELECT        lambert_scanner_results.arrival_position_x,       \
+                                                lambert_scanner_results.arrival_position_y,       \
+                                                lambert_scanner_results.arrival_position_z,       \
+                                                lambert_scanner_results.arrival_velocity_x,       \
+                                                lambert_scanner_results.arrival_velocity_y,       \
+                                                lambert_scanner_results.arrival_velocity_z        \
+                                  FROM          lambert_scanner_results                           \
+                                  INNER JOIN    sgp4_scanner_results                              \
+                                  ON            lambert_scanner_results.transfer_id =             \
+                                                sgp4_scanner_results.lambert_transfer_id          \
+                                  AND           sgp4_scanner_results.success = 1;",
+                                  database )
+
+lambert_scan_data.columns = [ 'arrivalPositionX',                                                 \
+                              'arrivalPositionY',                                                 \
+                              'arrivalPositionZ',                                                 \
+                              'arrivalVelocityX',                                                 \
+                              'arrivalVelocityY',                                                 \
+                              'arrivalVelocityZ', ]
+
+print "Fetch successful!"
+print ""
+
 # The histogram of the data
 xerror = scan_data[ 'xerror' ]
 yerror = scan_data[ 'yerror' ]
@@ -164,12 +188,126 @@ else:
     ycolor = '0.30'
     zcolor = '0.60'
 
+# ECI to RTN frame transformation.
+print ""
+print "Performing ECI to RTN frame conversion..."
+print ""
+
+def ECI2RTN( eciX, eciY, eciZ, refX, refY, refZ, refVX, refVY, refVZ ):
+  "This convertes a state in ECI frame to RTN frame"
+
+  # Radial unit vector:
+  unitR = np.zeros( shape=( 1, 3 ), dtype=float )
+
+  # Transverse unit vector:
+  unitT = np.zeros( shape=( 1, 3 ), dtype=float )
+
+  # Normal unit vector:
+  unitN = np.zeros( shape=( 1, 3 ), dtype=float )
+
+
+  vectorR = np.array( object=[ refX, refY, refZ ], dtype=float )
+  print "vectorR = ", vectorR
+
+  vectorV = np.array( object=[ refVX, refVY, refVZ ], dtype=float )
+  print "vectorV = ", vectorV
+
+  arrivalPositionMagnitude = np.linalg.norm( vectorR )
+
+  if arrivalPositionMagnitude != 0:
+    unitR = np.divide( vectorR, arrivalPositionMagnitude )
+  print "unit R = ", unitR
+
+  angularMomentumVector = np.cross( vectorR, vectorV )
+  angularMomentumMagnitude = np.linalg.norm( angularMomentumVector )
+
+  if angularMomentumMagnitude != 0:
+    unitN = np.divide( angularMomentumVector, angularMomentumMagnitude )
+  print "unit N = ", unitN
+
+  unitT = np.cross( unitN, unitR )
+  print "unit T = ", unitT
+
+  # Position expressed in ECI frame
+  vectorECI = np.zeros( shape=( 1, 3 ), dtype=float )
+  vectorECI[ 0 ][ 0 ] = eciX
+  vectorECI[ 0 ][ 1 ] = eciY
+  vectorECI[ 0 ][ 2 ] = eciZ
+  print "ECI = ", vectorECI
+
+  # Position expressed in RTN frame
+  vectorRTN = np.zeros( shape=( 3, 1 ), dtype=float )
+  vectorRTN[ 0 ] = np.inner( unitR, vectorECI )
+  vectorRTN[ 1 ] = np.inner( unitT, vectorECI )
+  vectorRTN[ 2 ] = np.inner( unitN, vectorECI )
+  print "RTN: "
+  print vectorRTN
+
+  return vectorRTN
+
+# Check for ECI to RTN converter
+# Case 1 from http://www.centerforspace.com/downloads/files/pubs/AAS-03-526.pdf
+eciX = -605.79221660e3
+eciY = -5870.22951108e3
+eciZ = 3493.05319896e3
+eciVX = -1.56825429e3
+eciVY = -3.70234891e3
+eciVZ = -6.47948395e3
+testVectorRTN = [ 0 for x in range( 0, 3 ) ]
+testVectorRTN = ECI2RTN( eciX, eciY, eciZ,                                                        \
+                         eciX + 6857.693605, eciY + 0.0, eciZ + 0.0,                              \
+                         eciVX + 0.007362813, eciVY + 7.62564531, eciVZ + 0.0 )
+
+# Case 2. circular equitorial orbit
+eciX = 10.0e3
+eciY = 0.0
+eciZ = 0.0
+testVectorRTN = ECI2RTN( eciX, eciY, eciZ, 0.0, 10.0e3, 0.0, -10.0, 0.0, 0.0 )
+exit( )
+
+arrivalPositionX = lambert_scan_data[ 'arrivalPositionX' ]
+arrivalPositionY = lambert_scan_data[ 'arrivalPositionY' ]
+arrivalPositionZ = lambert_scan_data[ 'arrivalPositionZ' ]
+arrivalVelocityX = lambert_scan_data[ 'arrivalVelocityX' ]
+arrivalVelocityY = lambert_scan_data[ 'arrivalVelocityY' ]
+arrivalVelocityZ = lambert_scan_data[ 'arrivalVelocityZ' ]
+
+totalCases = len( xerror )
+for i in tqdm( range( totalCases ) ):
+  vectorR = [ arrivalPositionX[ i ], arrivalPositionY[ i ], arrivalPositionZ[ i ] ]
+  vectorV = [ arrivalVelocityX[ i ], arrivalVelocityY[ i ], arrivalVelocityZ[ i ] ]
+  arrivalPositionMagnitude = np.linalg.norm( vectorR )
+  unitR = vectorR / arrivalPositionMagnitude
+
+  angularMomentumVector = np.cross( vectorR, vectorV )
+  angularMomentumMagnitude = np.linalg.norm( angularMomentumVector )
+  unitN = angularMomentumVector / angularMomentumMagnitude
+
+  unitT = np.cross( unitN, unitR )
+
+  gamma[ 0 ][ : ] = unitR
+  gamma[ 1 ][ : ] = unitT
+  gamma[ 2 ][ : ] = unitN
+
+  errorVectorECI = [ xerror[ i ], yerror[ i ], zerror[ i ] ]
+  errorVectorRTN = [ 0 for x in range( 0, 3 ) ]
+  errorVectorRTN[ 0 ] = np.inner( gamma[ 0 ][ : ], errorVectorECI )
+  errorVectorRTN[ 1 ] = np.inner( gamma[ 1 ][ : ], errorVectorECI )
+  errorVectorRTN[ 2 ] = np.inner( gamma[ 2 ][ : ], errorVectorECI )
+
+  xerror[ i ] = errorVectorRTN[ 0 ]
+  yerror[ i ] = errorVectorRTN[ 1 ]
+  zerror[ i ] = errorVectorRTN[ 2 ]
+
+print "ECI to RTN conversion completed!"
+print ""
+
 n, bins, patches = plt.hist( xerror, bins=200, histtype='step', normed=False, color=xcolor,       \
-                             alpha=1, label='X Axis', log=False )
+                             alpha=1, label='R Axis', log=False )
 n, bins, patches = plt.hist( yerror, bins=200, histtype='step', normed=False, color=ycolor,       \
-                             alpha=1, label='Y Axis', log=False )
+                             alpha=1, label='T Axis', log=False )
 n, bins, patches = plt.hist( zerror, bins=200, histtype='step', normed=False, color=zcolor,       \
-                             alpha=1, label='Z Axis', log=False )
+                             alpha=1, label='N Axis', log=False )
 # Figure properties
 plt.xlabel( 'Error' + " " + errorUnit )
 plt.ylabel( 'Frequency' )
@@ -193,9 +331,9 @@ if xAxesLowerLimit != 0                                                         
                   yAxesLowerLimit,                                                                \
                   yAxesUpperLimit])
 
-xlegend = mlines.Line2D( [], [], color=xcolor, label='X Axis' )
-ylegend = mlines.Line2D( [], [], color=ycolor, label='Y Axis' )
-zlegend = mlines.Line2D( [], [], color=zcolor, label='Z Axis' )
+xlegend = mlines.Line2D( [], [], color=xcolor, label='R Axis' )
+ylegend = mlines.Line2D( [], [], color=ycolor, label='T Axis' )
+zlegend = mlines.Line2D( [], [], color=zcolor, label='N Axis' )
 lines = [xlegend, ylegend, zlegend]
 labels = [line.get_label( ) for line in lines]
 plt.legend(lines, labels)
