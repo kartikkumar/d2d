41dc712f6c2aefa06924bc233b5f919e520c10af
Enne Hekma <ennehekma@gmail.com>
2016-05-12 15:17:31 +0200

    small change

--- python/plot_histogram_dV.py
+++ python/plot_histogram_dV.py
@@ -92,7 +92,7 @@ scan_data.columns = [ 'transfer_delta_v' ]
 x = scan_data[ 'transfer_delta_v']
 # print x
 # (mu, sigma) = norm.fit(x)
-# n, bins, patches = plt.hist( x, bins=50, facecolor='grey', alpha=0.75, normed=1)
+n, bins, patches = plt.hist( x, bins=50, facecolor='grey', alpha=0.75)
 
 # y = mlab.normpdf( bins, mu, sigma)
 # l = plt.plot(bins, y, 'r--', linewidth=2)
@@ -104,7 +104,7 @@ plt.ylabel('Frequency')
 plt.ticklabel_format(style='sci', axis='y', scilimits=(0,0))
 # plt.title(r'$\mathrm{Histogram\ of\ IQ:}\ \mu=100,\ \sigma=15$')
 # plt.axis([40, 160, 0, 0.03])
-plt.title(r'$\mathrm{Histogram\ of\ IQ:}\ \mu=%.3f,\ \sigma=%.3f$' %(mu, sigma))
+# plt.title(r'$\mathrm{Histogram\ of\ IQ:}\ \mu=%.3f,\ \sigma=%.3f$' %(mu, sigma))
 plt.grid(True)
 
 # Save figure to file.
